ddf96ba3b330c3deeb475d9bfad05f8a12d949cd
hoonee <cjhoon5@naver.com>
2022-07-29 17:10:00 +0900

    vue3 listCycle

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        count:2
+      }
+    },
+    beforeCreate(){
+      console.log('beforeCreate!', this.count)
+    },
+    created(){
+      console.log('Created!', this.count)
+    },
+    beforeMount(){
+      console.log('beforeMount!')
+    },
+    mounted() {
+      console.log('mounted!')
+      console.log(document.querySelector('h1'))
+    },
+  }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <img 
+    src="~assets/test.png"
+    alt="test" 
+    />
+    <div />
+</template>
